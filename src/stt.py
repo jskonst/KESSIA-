4583e6a5e205138ac9693bdca7307c98fa69541c
jskonst <jskonst@yandex.ru>
2022-11-18 02:07:39 +0300

    full flow

--- src/stt.py
+++ src/stt.py
@@ -5,24 +5,26 @@ import config
 FOLDER_ID = config.folder_id
 IAM_TOKEN = config.voice_token
 
-with open("test.ogg", "rb") as f:
-    data = f.read()
+# TODO - cleanup
+# with open("test.ogg", "rb") as f:
+#     data = f.read()
 
 
-params = "&".join([
+def stt(data):
+    params = "&".join([
     "topic=general",
     "folderId=%s" % FOLDER_ID,
-    "lang=ru-RU"
-])
+    "lang=ru-RU",
+    "format=lpcm",
+    "sampleRateHertz=16000",
+    ])
+    headers = {
+        'Authorization': 'Bearer ' + IAM_TOKEN,
+    }
+    url = f"https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?{params}"
 
-url = f"https://stt.api.cloud.yandex.net/speech/v1/stt:recognize?{params}"
-
-headers = {
-    'Authorization': 'Bearer ' + IAM_TOKEN,
-}
-with requests.post(url, data=data, headers=headers, stream=True) as resp:
-    if resp.status_code != 200:
-        raise RuntimeError("Invalid response received: code: %d, message: %s" % (resp.status_code, resp.text))
-    print(resp.json())
-    print(resp.json().get("result", ""))
-
+    with requests.post(url, data=data, headers=headers, stream=True) as resp:
+        if resp.status_code != 200:
+            raise RuntimeError("Invalid response received: code: %d, message: %s" % (resp.status_code, resp.text))
+        result = resp.json().get("result", "")
+        return result
--- src/tts.py
+++ src/tts.py
@@ -1,5 +1,7 @@
 import requests
 import config
+import pyaudio
+import wave
 
 def synthesize(folder_id, iam_token, text):
     url = 'https://tts.api.cloud.yandex.net/speech/v1/tts:synthesize'
@@ -9,24 +11,39 @@ def synthesize(folder_id, iam_token, text):
 
     data = {
         'text': text,
-        'lang': 'ru-RU',
-        'voice': 'filipp',
+        'lang': 'en-EN', # вынести в параметр
+        'voice': 'john', # и это тоже 'filipp',
         'folderId': folder_id,
         'format': 'lpcm',
-        'sampleRateHertz': 48000,
+        'sampleRateHertz': 8000,
     }
 
     with requests.post(url, headers=headers, data=data, stream=True) as resp:
         if resp.status_code != 200:
             raise RuntimeError("Invalid response received: code: %d, message: %s" % (resp.status_code, resp.text))
 
-        for chunk in resp.iter_content(chunk_size=None):
+        for chunk in resp.iter_content(chunk_size=None):            
             yield chunk
+
 
 
 if __name__ == "__main__":
     str_to_generate = "Проверка генерации текста"
     filename = "out.raw"
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16,
+                channels=1,
+                rate=8000,
+                output=True)
+
+
     with open(filename, "wb") as f:
         for audio_content in synthesize(config.folder_id, config.voice_token, str_to_generate):
+            stream.write(audio_content)
             f.write(audio_content)
+    
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
